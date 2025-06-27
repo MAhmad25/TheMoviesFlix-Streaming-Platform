d14bf6f3919c7658437ac5770da05d07139d3356
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-06-27 18:01:55 +0500

    feat: integrate SpeedInsights component into App and adjust return structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import LocomotiveScroll from "locomotive-scroll";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 const App = () => {
       new LocomotiveScroll();
-      <SpeedInsights />;
-      return <RouterHandler />;
+      return (
+            <>
+                  <SpeedInsights />
+                  <RouterHandler />
+            </>
+      );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
-
 createRoot(document.getElementById("root")).render(
       <Provider store={store}>
             <BrowserRouter>
