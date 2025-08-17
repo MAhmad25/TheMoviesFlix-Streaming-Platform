fdfe4f54497516dadec9238d8263b9ab13906d5f
MAhmad25 <userarenotproduct@gmail.com>
2025-08-18 00:32:26 +0500

    Fixes some Ui in Trailer

--- src/Components/pages/Trailer.jsx
+++ src/Components/pages/Trailer.jsx
@@ -7,14 +7,14 @@ import useFullScreen from "../../hooks/useFullScreen";
 
 const Trailer = () => {
       useFullScreen();
-      let { pathname } = useLocation();
+      const { pathname } = useLocation();
       const isMovie = pathname.includes("movie") ? "movie" : "tv";
       const video = useSelector((state) => state[isMovie].info.videoLink);
       return (
             <>
                   {video ? (
-                        <motion.section initial={{ scale: 0 }} animate={{ scale: 1 }} className="w-screen no-scroll fixed inset-0  flex justify-center items-center  bg-black/70 backdrop-blur-sm h-screen">
-                              <div className="w-full mb-44 sm:w-[80%] sm:mb-0 h-80 sm:h-[25rem] min-[1250px]:h-[27rem]">
+                        <motion.section initial={{ scale: 0 }} animate={{ scale: 1 }} className="w-screen no-scroll fixed inset-0 flex justify-center items-center  bg-black/70 backdrop-blur-sm h-screen">
+                              <div className="w-full mb-44 sm:w-[80%] sm:mb-0 h-80 sm:h-[25rem] min-[1250px]:h-[27rem] overflow-hidden rounded-2xl">
                                     <ReactPlayer url={`https://www.youtube.com/watch?v=${video.key}`} height="100%" width="100%" />
                               </div>
                         </motion.section>
