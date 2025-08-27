0e99bdaf4e26ec8bea0360a2690f5cea76fe6dfd
MAhmad25 <userarenotproduct@gmail.com>
2025-08-28 01:23:48 +0500

    There is some Performance issue

--- src/Components/ui/Loaders/ImageLoader.jsx
+++ src/Components/ui/Loaders/ImageLoader.jsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 const ImageLoader = () => {
       return (
-            <motion.div exit={{ opacity: 0, filter: "blur(10px)" }} transition={{ duration: 0.3, ease: "easeInOut" }} className="w-full  h-full rounded-lg shadow animate-pulse  ">
+            <motion.div exit={{ y: -100, filter: "blur(10px)" }} transition={{ duration: 0.3, ease: "easeInOut" }} className="w-full  h-full rounded-lg shadow animate-pulse  ">
                   <div className="flex items-center justify-center w-full h-full bg-[#1B3E3A] rounded">
                         <svg viewBox="0 0 16 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="w-10 h-10 text-white ">
                               <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
