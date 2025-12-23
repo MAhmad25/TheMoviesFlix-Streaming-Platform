232efc24c38a0f0d1b01e596bc1ad45ca66363af
MAhmad25 <userarenotproduct@gmail.com>
2025-12-24 00:41:49 +0500

    some fixes you will not understand

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const instance = axios.create({
-      baseURL: "https://api.themoviedb.org/3/",
+      baseURL: import.meta.env.VITE_BASE_URL,
       headers: {
             accept: "application/json",
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNmEwODE2YmZjZGE1ZmYxNjhhZTZlNDYwNTYzMDk4MCIsIm5iZiI6MTczOTAwNzU0My45NjYsInN1YiI6IjY3YTcyNjM3ZTRiZDAyZTY0Y2UwNWY0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3ZS5YWe6AGyN02S9QfckvTO2onYOXAeleexmECC0WDY",
+            Authorization: `Bearer ${import.meta.env.VITE_API_SECRET}`,
       },
 });
 export default instance;
