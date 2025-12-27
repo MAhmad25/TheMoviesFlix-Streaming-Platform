35f06cc6f7d6ba22cc6fda5ba0377f9785d5adc7
MAhmad25 <userarenotproduct@gmail.com>
2025-12-27 13:48:01 +0500

    some more changes

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { TrendingContainer, Header, ComingSoonContainer } from "../ui/index";
 const Home = () => {
-      document.title = "MoviesFlix | Home - Watch and Stream Movies and TV Series";
       return (
             <div className="w-full [background-image:var(--bg-gradient)] relative overflow-hidden font-Stoshi min-h-full sm:pb-10 md:pb-0 sm:mb-0">
                   <Header />
