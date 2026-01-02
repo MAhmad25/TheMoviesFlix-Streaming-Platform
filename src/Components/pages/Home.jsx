2adf537dea2df386946f4785f21f3cdf85014477
MAhmad25 <userarenotproduct@gmail.com>
2026-01-02 14:57:36 +0500

    title added in home screen

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { TrendingContainer, Header, ComingSoonContainer } from "../ui/index";
 const Home = () => {
+      document.title = "MoviesFlix | Movie & TV Streaming ";
       return (
             <div className="w-full [background-image:var(--bg-gradient)] relative overflow-hidden font-Stoshi min-h-full sm:pb-10 md:pb-0 sm:mb-0">
                   <Header />
