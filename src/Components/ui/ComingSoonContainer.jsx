6f9d1757c114f321af3bcba64268a17f66797299
MAhmad25 <userarenotproduct@gmail.com>
2026-01-09 01:37:24 +0500

    improved the slider with premium look

--- src/Components/ui/ComingSoonContainer.jsx
+++ src/Components/ui/ComingSoonContainer.jsx
@@ -15,7 +15,7 @@ const ComingSoonContainer = () => {
       }, []);
 
       return (
-            <section className="w-full mb-28 relative  px-5">
+            <section className="w-full mb-28 sm:mb-10 relative  px-5">
                   <div className="flex w-full mb-3 justify-center items-center">
                         <span className="w-full  border-dotted border-[1px] border-gray-400"></span>
                         <h1 className="sm:text-3xl text-nowrap  text-[#fefefe] text-xl">Now Playing</h1>
