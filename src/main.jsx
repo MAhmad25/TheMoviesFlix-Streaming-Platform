6c92f1d373c413bc087e542e9037fa0ded0f6239
MAhmad25 <userarenotproduct@gmail.com>
2026-01-30 23:14:13 +0500

    Remove analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.jsx";
-import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")).render(
-      <Provider store={store}>
-            <BrowserRouter>
-                  <Analytics />
-                  <App />
-            </BrowserRouter>
-      </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
