4ff1e01b01ea056c7bf20e58db770e102648daa0
MAhmad25 <userarenotproduct@gmail.com>
2026-02-25 07:09:41 +0500

    trying

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
       plugins: [react()],
+      server: {
+            proxy: {
+                  "/tmdb": {
+                        target: "https://image.tmdb.org",
+                        changeOrigin: true,
+                        rewrite: (path) => path.replace(/^\/tmdb/, ""),
+                  },
+            },
+      },
 });
