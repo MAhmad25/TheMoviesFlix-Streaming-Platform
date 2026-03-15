d2b2c4cffbb7ca83fc02f14f61f5eaebb45f3e02
MAhmad25 <userarenotproduct@gmail.com>
2026-03-15 23:47:12 +0500

    add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import RouterHandler from "../src/routes/RouterHandler";
+import { ReactLenis, useLenis } from "lenis/react";
 const App = () => {
-      return <RouterHandler />;
+      useLenis();
+      return (
+            <div className="bg-[#300b07]">
+                  <ReactLenis root />
+                  <RouterHandler />;
+            </div>
+      );
 };
 
 export default App;
